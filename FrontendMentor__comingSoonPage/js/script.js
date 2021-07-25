69e2162d786502634beabb05b4a1cec0eddd81fd
Huseyn <85623387+Huseyn-SomeGuy@users.noreply.github.com>
2021-07-25 11:47:22 +0400

    Add files via upload

---
+++ FrontendMentor__comingSoonPage/js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let searchBlock = document.querySelector(".input");
+let searchBlockButton = document.querySelector(".button--hover");
+let errorMessage = document.querySelector(".error-message");
+let errorSymbol = document.querySelector(".error-symbol");
+
+
+searchBlockButton.addEventListener("click", function (){
+   try{
+      if(!searchBlock.value.includes("@")) {throw "Please provide a valid email"};
+   }
+
+   catch (err){
+      errorMessage.innerHTML = err;
+      errorSymbol.style.display = "block";
+      searchBlock.style.border = "1px solid red";
+   }
+
+   finally {
+      searchBlock.value = "";
+   }
+})
